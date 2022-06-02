26d6d49e4a6fd78b9cd3be2999c70fc90fe9d19e
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 14:18:23 +0200

    feat: add home page component tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,24 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { By } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  describe('#component tests', () => {
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [MatCardModule],
+        declarations: [HomeComponent],
+      }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HomeComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should display home title', () => {
+      const title = fixture.debugElement.query(
+        By.css('[data-test-id="title"]')
+      );
+
+      expect(title.nativeElement.textContent).toContain('Home');
+    });
+
+    it('should display home content', () => {
+      const content = fixture.debugElement.query(
+        By.css('[data-test-id="content"]')
+      );
+
+      expect(content.nativeElement.textContent).toContain(
+        'Welcome to home page'
+      );
+    });
   });
 });
