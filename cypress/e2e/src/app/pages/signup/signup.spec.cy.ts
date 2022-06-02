4a5a23bb18c6722b6a6937cecc3badcfdf170f47
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 19:25:27 +0200

    feat: update e2e test description

--- cypress/e2e/src/app/pages/signup/signup.spec.cy.ts
+++ cypress/e2e/src/app/pages/signup/signup.spec.cy.ts
@@ -7,7 +7,7 @@ describe('Signup page', () => {
     cy.contains('Sign Up');
   });
 
-  it('should display 4 errors', () => {
+  it('should display 4 required errors if forms inputs are not set', () => {
     cy.get('[data-test-id="submit-button"]').click();
 
     cy.get('mat-error').should('have.length', 4);
