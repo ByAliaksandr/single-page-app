d87c12e75fac0d0378cdf2060601c0f956612f72
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 14:49:42 +0200

    feat: set signup-form test structure

--- src/app/pages/signup/signup-form/signup-form.component.spec.ts
+++ src/app/pages/signup/signup-form/signup-form.component.spec.ts
@@ -1,24 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SignupFormComponent } from './signup-form.component';
 
 describe('SignupFormComponent', () => {
-  let component: SignupFormComponent;
-  let fixture: ComponentFixture<SignupFormComponent>;
+  // describe('#unit tests', () => {});
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignupFormComponent],
-    }).compileComponents();
-  });
+  describe('#component tests', () => {
+    let component: SignupFormComponent;
+    let fixture: ComponentFixture<SignupFormComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignupFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          BrowserAnimationsModule,
+          ReactiveFormsModule,
+          MatInputModule,
+          MatIconModule,
+          MatButtonModule,
+        ],
+        declarations: [SignupFormComponent],
+      }).compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SignupFormComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
