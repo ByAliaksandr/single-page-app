32ed236490fad9e87f4d08eeaceae680c5a3b3c7
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 14:37:19 +0200

    feat: add signup page component tests

--- src/app/pages/signup/signup.component.spec.ts
+++ src/app/pages/signup/signup.component.spec.ts
@@ -1,24 +1,49 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { By } from '@angular/platform-browser';
 
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+  describe('#component tests', () => {
+    let component: SignupComponent;
+    let fixture: ComponentFixture<SignupComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignupComponent],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [MatCardModule],
+        declarations: [SignupComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SignupComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should display signup title', () => {
+      const title = fixture.debugElement.query(
+        By.css('[data-test-id="title"]')
+      );
+
+      expect(title.nativeElement.textContent).toContain('Sign Up');
+    });
+
+    it('should display signup content', () => {
+      const content = fixture.debugElement.query(
+        By.css('[data-test-id="content"]')
+      );
+
+      const appSignupForm = content.query(By.css('app-signup-form'));
+
+      expect(appSignupForm).toBeDefined();
+    });
   });
 });
