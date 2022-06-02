4236a41bc18266a3dd2a79ee279af793a138b8fa
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 13:05:04 +0200

    feat: finalize signup page design

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
 import { SignupFormModule } from './signup-form/signup-form.module';
 
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [SignupComponent],
-  imports: [CommonModule, SignupFormModule],
+  imports: [CommonModule, MatCardModule, SignupFormModule],
 })
 export class SignupModule {}
