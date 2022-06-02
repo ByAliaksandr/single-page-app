e0fdb9e692056ce7c3dd9cedc30034e631dda0da
Aliaksandr <aliaksandr.bytsai@gmail.com>
2022-06-02 17:02:57 +0200

    feat: add cypress tests

---
+++ cypress/e2e/src/app/pages/signup/signup.spec.cy.ts
@@ -0,0 +1,26 @@
+describe('Signup page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should open the signup page', () => {
+    cy.contains('Sign Up');
+  });
+
+  it('should display 4 errors', () => {
+    cy.get('[data-test-id="submit-button"]').click();
+
+    cy.get('mat-error').should('have.length', 4);
+  });
+
+  it('should navigate to home page', () => {
+    cy.get('[formcontrolname="firstname"]').type('Aaa');
+    cy.get('[formcontrolname="lastname"]').type('Bbb');
+    cy.get('[formcontrolname="email"]').type('aaa@bbb');
+    cy.get('[formcontrolname="password"]').type('ddddDDDD');
+
+    cy.get('[data-test-id="submit-button"]').click();
+
+    cy.location('pathname').should('include', '/home');
+  });
+});
